7b4a3a10cd08634fa1bd580c2237deb981495601
Oshioke Salaki <salaki1902@gmail.com>
2024-02-11 22:18:39 +0000

    feat: implement bwc balance fetching

--- dapp/src/ui/layout/AppLayout.jsx
+++ dapp/src/ui/layout/AppLayout.jsx
@@ -2,53 +2,17 @@ import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { connect, disconnect } from "starknetkit";
+import { useConnectWallet } from "../../context/ConnectContext";
 
 function AppLayout() {
-  const [connection, setConnection] = useState("");
-  const [provider, setProvider] = useState("");
-  const [address, setAddress] = useState("");
 
-  useEffect(() => {
-    const connectToStarknet = async () => {
-      const connection = await connect({ modalMode: "neverAsk" });
-
-      if (connection && connection.isConnected) {
-        setConnection(connection);
-        setProvider(connection.account);
-        setAddress(connection.selectedAddress);
-      }
-    };
-
-    connectToStarknet();
-  }, []);
-
-  const connectWallet = async () => {
-    const connection = await connect();
-
-    if (connection && connection.isConnected) {
-      setConnection(connection);
-      setProvider(connection.account);
-      setAddress(connection.selectedAddress);
-    }
-  };
-
-  const disconnectWallet = async () => {
-    await disconnect();
-
-    setConnection(undefined);
-    setProvider(undefined);
-    setAddress("");
-  };
+  const {connection} = useConnectWallet()
 
   return (
     <div className="flex min-h-[100vh] w-full flex-col bg-mainBg bg-cover bg-center bg-no-repeat pt-[140px]">
       <Navbar
-        connection={connection}
-        connectWallet={connectWallet}
-        disconnectWallet={disconnectWallet}
-        address={address}
       />
-      {connection && <Outlet context={[provider, address]} />}
+      {connection && <Outlet />}
     </div>
   );
 }
